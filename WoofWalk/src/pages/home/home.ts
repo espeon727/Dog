0c59253cc5b619aa46ade255d0fc155ea10735d0
Amelia Miller <millea195@gmail.com>
2017-02-22 21:54:36 -0500

    Added Demon Dog image to toggle Moved Dog to bottom of screen

--- WoofWalk/src/pages/home/home.ts
+++ WoofWalk/src/pages/home/home.ts
@@ -9,16 +9,16 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    
+
   }
-  
+
   clicked(){
-   if (document.getElementById('dogpic').getAttribute("src") == "../assets/images/DogPixelArt.png" )
+   if (document.getElementById('dogpic').getAttribute("src") == "../assets/images/dpa_02_transparent_bg_Large.png" )
    {
      document.getElementById('dogpic').setAttribute("src", "../assets/images/dpa_01_transparent_bg_Large.png");
    } else
    {
-     document.getElementById('dogpic').setAttribute("src", "../assets/images/DogPixelArt.png");
+     document.getElementById('dogpic').setAttribute("src", "../assets/images/dpa_02_transparent_bg_Large.png");
    }
 }
 
