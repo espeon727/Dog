b5da4c287ff7a6ab51ff1a9fbb07afcf1b020eb3
alexgentile <agentile123@gmail.com>
2017-02-22 21:31:21 -0500

    Made the image change on button push
    Made the image change on button push

--- WoofWalk/src/pages/home/home.ts
+++ WoofWalk/src/pages/home/home.ts
@@ -13,16 +13,13 @@ export class HomePage {
   }
   
   clicked(){
-   
-   if (document.getElementById('dogpic').getAttribute("src") == "../www/assets/images/DogPixelArt.png" )
+   if (document.getElementById('dogpic').getAttribute("src") == "../assets/images/DogPixelArt.png" )
    {
-     document.getElementById('dogpic').getAttribute("src").replace("../www/assets/images/DogPixelArt.png","../www/assets/images/dpa_01_transparent_bg_Large.png");
+     document.getElementById('dogpic').setAttribute("src", "../assets/images/dpa_01_transparent_bg_Large.png");
    } else
    {
-     document.getElementById('dogpic').getAttribute("src").replace("../www/assets/images/dpa_01_transparent_bg_Large.png","../www/assets/images/DogPixelArt.png");
+     document.getElementById('dogpic').setAttribute("src", "../assets/images/DogPixelArt.png");
    }
-   
-   console.log("CLICKED");
 }
 
 }
