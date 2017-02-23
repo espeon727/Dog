e49267aa8318b9f210893cfa6321f0884a1beda0
CatBlackthorn <horsewarrior@gotsky.com>
2017-02-22 21:05:18 -0500

    Fixed Problem. Can click

--- WoofWalk/src/pages/home/home.ts
+++ WoofWalk/src/pages/home/home.ts
@@ -13,13 +13,16 @@ export class HomePage {
   }
   
   clicked(){
-   if (document.getElementById('dogpic').src == "../www/assets/images/DogPixelArt.png" )
+   
+   if (document.getElementById('dogpic').getAttribute("src") == "../www/assets/images/DogPixelArt.png" )
    {
-     document.getElementById('dogpic').src = "../www/assets/images/DogPixelArtNew.png";
+     document.getElementById('dogpic').getAttribute("src").replace("../www/assets/images/DogPixelArt.png","../www/assets/images/dpa_01_transparent_bg_Large.png");
    } else
    {
-     document.getElementById('dogpic').src = "../www/assets/images/DogPixelArt.png";
+     document.getElementById('dogpic').getAttribute("src").replace("../www/assets/images/dpa_01_transparent_bg_Large.png","../www/assets/images/DogPixelArt.png");
    }
+   
+   console.log("CLICKED");
 }
 
 }
