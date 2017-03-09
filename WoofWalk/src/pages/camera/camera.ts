78023c957b5c14f329ab1db19158443c9579e28d
cyee13 <cyee13@punahou.edu>
2017-03-08 21:41:50 -0500

    basic camera functionality

--- WoofWalk/src/pages/camera/camera.ts
+++ WoofWalk/src/pages/camera/camera.ts
@@ -1,22 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Camera } from 'ionic-native';
 
 /*
-  Generated class for the Camera page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
+  Used this tutorial 
+  https://www.thepolyglotdeveloper.com/2016/04/use-the-device-camera-in-an-ionic-2-android-and-ios-app/
 */
 @Component({
   selector: 'page-camera',
   templateUrl: 'camera.html'
 })
 export class CameraPage {
+	public base64Image: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  {
+  	this.base64Image = "../www/assets/images/dpa_02_transparent_bg_large.png"
+  }
 
-  ionViewDidLoad() {
+  ionViewDidLoad() 
+  {
     console.log('ionViewDidLoad CameraPage');
+  }
+
+  public takePicture() 
+  {
+  	Camera.getPicture(
+  	{
+  		quality: 75,
+  		destinationType: Camera.DestinationType.DATA_URL,
+  		sourceType: Camera.PictureSourceType.CAMERA,
+  		allowEdit: true,
+  		encodingType: Camera.EncodingType.JPEG,
+  		targetWidth: 300,
+  		targetHeight: 300,
+  		saveToPhotoAlbum: false
+  	}).then(imageData => 
+  	{
+  		this.base64Image = "data:image/jpeg;base64," + imageData;
+  	}, error =>
+  	{
+  		alert("Error -> " + JSON.stringify(error));
+  	});
   }
 
 }
