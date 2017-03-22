5f70074ccb47a94d26a20dca0045396a1f323b11
carlsc3 <me@example.com>
2017-03-21 21:34:59 -0400

    Geolocation: ADD cordova plugin add cordova-plugin-geolocation  Will work

--- WoofWalk/src/pages/walk/walk.ts
+++ WoofWalk/src/pages/walk/walk.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
+
+declare var google;
+
 
 /*
   Generated class for the Walk page.
@@ -15,8 +19,49 @@ export class WalkPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad WalkPage');
+	this.loadMap();
   }
 
+  loadMap() {
+   /* 
+	let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+   */
+ 
+	Geolocation.getCurrentPosition().then((position) => {
+	  
+	  let latLng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+	  
+	  let mapOptions = {
+	    center: latLng,
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	  }
+	  
+	  var map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+	  
+	  var myLocation = new google.maps.Marker({
+        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        map: map,
+		title: "My Location"
+      });
+	  this.map = map;
+	}, (err) => {
+	  console.log(err);
+	});
+	
+  }
+  
 }
