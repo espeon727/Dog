4977142067c7265ab20dc73eddde44fa05b48eba
Amelia Miller <millea195@gmail.com>
2017-03-21 21:53:35 -0400

    Added image assets to app, inventory updated to loop through simulated retrieved data

--- WoofWalk/src/pages/inventory/inventory.ts
+++ WoofWalk/src/pages/inventory/inventory.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams } from 'ionic-angular';
 
 /*
   Generated class for the Inventory page.
@@ -7,16 +7,28 @@ import { NavController, NavParams } from 'ionic-angular';
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+
 @Component({
   selector: 'page-inventory',
   templateUrl: 'inventory.html'
 })
 export class InventoryPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad InventoryPage');
+  private nav
+  private results
+  static get parameters() {
+    return [[NavController]];
   }
 
+  constructor(nav) {
+    this.nav = nav;
+    this.results = this.getResults();
+  }
+
+  getResults() {
+      return [
+        {"name": "Bone", "quantity": 12, "icon": "../www/assets/images/bone_normal.png", "icon2": "../assets/images/bone_normal.png"},
+        {"name": "Fancy Bone", "quantity": 13, "icon": "../www/assets/images/bone_fancy.png", "icon2": "../assets/images/bone_normal.png"},
+        {"name": "Canned Food", "quantity": 14, "icon": "../www/assets/images/food_can.png", "icon2": "../assets/images/bone_normal.png"},
+      ];
+  }
 }
